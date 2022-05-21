02fc9071542f9ef9971434896d9421c67a144fc7
Brownoak <helanii93@gmail.com>
2022-05-21 10:19:42 -0700

    search data function in loader

---
+++ ObjLoader.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class ObjLoader:
+    buffer = []
+
+    @staticmethod
+    def search_data(data_values, coordinates, skip, data_type):
+        for d in data_values:
+            if d == skip:
+                continue
+            if data_type == 'float':
+                coordinates.append(float(d))
+            elif data_type == 'int':
+                coordinates.append(int(d)-1)
+
--- README.md
+++ README.md
@@ -7,9 +7,11 @@ Our project is intended to provide a 3D water fountain design tool that any user
 ### feature
 It come up with digitalized view of the simple water fountain structure in general.
 
-
+# Code References:-
+### https://github.com/yarolig/OBJFileLoader 
 
 ## Group Members
 
 1. Helawit Abraham (UGR/2966/12)
 2. Hiwot Derese (UGR/2790/12)
+
