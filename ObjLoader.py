a64082837b77533df79b02888d15b5c4ecc45443
Brownoak <helanii93@gmail.com>
2022-05-25 20:24:54 -0700

    unsorted vertex buffer in loader

--- ObjLoader.py
+++ ObjLoader.py
@@ -14,3 +14,25 @@ class ObjLoader:
             elif data_type == 'int':
                 coordinates.append(int(d)-1)
 
+
+    @staticmethod # TODO unsorted vertex buffer for use with glDrawElements function
+    def create_unsorted_vertex_buffer(indices_data, vertices, textures, normals):
+        num_verts = len(vertices) // 3
+
+        for i1 in range(num_verts):
+            start = i1 * 3
+            end = start + 3
+            ObjLoader.buffer.extend(vertices[start:end])
+
+            for i2, data in enumerate(indices_data):
+                if i2 % 3 == 0 and data == i1:
+                    start = indices_data[i2 + 1] * 2
+                    end = start + 2
+                    ObjLoader.buffer.extend(textures[start:end])
+
+                    start = indices_data[i2 + 2] * 3
+                    end = start + 3
+                    ObjLoader.buffer.extend(normals[start:end])
+
+                    break
+
