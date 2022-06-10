5b6834c250295e7b72cc63da65defdb8c6d4f7e1
Hiwot Derese <Hiwotderese16@gmail.com>
2022-06-10 14:52:47 +0300

    basic

---
+++ flowing.py
@@ -0,0 +1,24 @@
+import pygame
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from pygame.locals import *
+
+def init():
+    pygame.init()
+    display = (500, 500)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    glClearColor(0.0, 0.0, 0.0, 1.0)
+    gluOrtho2D(-1.0, 1.0, -1.0, 1.0)
+
+def draw():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glColor3f(1.0, 0.0, 0.0)
+
+    glBegin(GL_POINTS)
+    
+    glVertex2f(0.0, 0.0)
+
+    glEnd()
+    glFlush()
+
+    
